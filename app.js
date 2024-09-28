854448751ce44738c7dad5d35067c5a6d3f5cc88
karan-chand <karan0chand@gmail.com>
2024-09-28 19:39:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -115,4 +115,4 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-});  
+});

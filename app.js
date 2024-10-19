210e8c530ba9c712f80b36e57a325d25f3951fe5
karan-chand <karan0chand@gmail.com>
2024-10-19 20:32:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const bloomPass = new THREE.UnrealBloomPass(
     new THREE.Vector2(window.innerWidth, window.innerHeight),
     1.0,  // Base bloom strength
     0.4,  // Base bloom radius
-    0.85  // Threshold of brightness to apply the bloom effect
+    0.15  // Threshold of brightness to apply the bloom effect
 );
 composer.addPass(bloomPass);
 
@@ -40,8 +40,8 @@ const starData = [
 ];
 
 let starMeshes = [];
-const baseEmissiveIntensity = 0.8;
-const hoverEmissiveMultiplier = 1.618;
+const baseEmissiveIntensity = 0.1;
+const hoverEmissiveMultiplier = 16.18;
 const clickEmissiveMultiplier = 10;
 let currentlyHoveredStar = null;
 

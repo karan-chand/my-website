98083639a1fb3671e2c5ade6b896677880b3d656
karan-chand <karan0chand@gmail.com>
2024-11-17 02:44:15 +0000

    Update app.js

--- app.js
+++ app.js
@@ -41,10 +41,10 @@ const starData = [
     { name: 'Porrima', x: 2.19, y: 0.686, z: -0.09, size: 0.5 },   // Gamma Virginis
     { name: 'Auva', x: 1.35, y: 1.295, z: -7.23, size: 0.4 },     // Delta Virginis
     { name: 'Vindemiatrix', x: 0.92, y: 2.892, z: -2.83, size: 0.6 }, // Epsilon Virginis
-    { name: 'Heze', x: -2.36, y: 0.394, z: 1.41, size: -0.35 },    // Zeta Virginis
-    { name: 'Zaniah', x: 3.73, y: 0.348, z: 8.98, size: -0.4 },    // Eta Virginis
+    { name: 'Heze', x: -2.36, y: 0.394, z: -1.41, size: 0.35 },    // Zeta Virginis
+    { name: 'Zaniah', x: 3.73, y: 0.348, z: -8.98, size: 0.4 },    // Eta Virginis
     { name: 'Theta Virginis', x: 0.40, y: -1.466, z: -11.51, size: 0.3 }, // Theta Virginis
-    { name: 'Syrma', x: -4.00, y: -1.530, z: 1.27, size: -0.35 },   // Iota Virginis
+    { name: 'Syrma', x: -4.00, y: -1.530, z: -1.27, size: 0.35 },   // Iota Virginis
     { name: 'Rijl al Awwa', x: -5.79, y: -1.466, z: -0.95, size: 0.35 }, // Mu Virginis
     { name: 'Nu Virginis', x: 6.00, y: 1.643, z: -12.0, size: 0.3 }, // Nu Virginis
     { name: 'Omicron Virginis', x: 5.29, y: 2.157, z: -4.79, size: 0.3 }, // Omicron Virginis
